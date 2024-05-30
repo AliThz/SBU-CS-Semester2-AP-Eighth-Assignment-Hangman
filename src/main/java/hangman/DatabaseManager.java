5c33919fdaf541f4100f7c03c794061a488dabed
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-05-30 17:49:24 +0330

    selectUserInfos method implemented

--- src/main/java/hangman/DatabaseManager.java
+++ src/main/java/hangman/DatabaseManager.java
@@ -1,8 +1,49 @@
 package hangman;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 // Use JDBC to connect to your database and run queries
 
 public class DatabaseManager {
+    public static void main(String[] args) {
+    }
+    //region [ - UserInfo CRUD - ]
+
+    //region [ - ArrayList<UserInfo> selectUserInfos() - ]
+    public ArrayList<UserInfo> selectUserInfos() {
+        Connection c;
+        Statement stmt;
+        ArrayList<UserInfo> userInfos = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:4321/SBU-CS-Semester2-AP-Eighth-Assignment-Hangman", "postgres", "hmhat");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully (selectUserInfos)");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM UserInfo;");
+            userInfos = new ArrayList<>();
+            while (rs.next()) {
+                UserInfo userInfo = new UserInfo();
+                userInfo.setName(rs.getString("Name"));
+                userInfo.setUsername(rs.getString("Username"));
+                userInfo.setPassword(rs.getString("Password"));
+                userInfos.add(userInfo);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully (selectUserInfos)");
+        return userInfos;
+    }
+    //endregion
+
+
+
+    //endregion
 }
