69346f927e397f1141f17065c2c272e9a090b752
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-06-03 22:32:40 +0330

    minor changes

--- src/main/java/hangman/SignUpController.java
+++ src/main/java/hangman/SignUpController.java
@@ -2,6 +2,7 @@ package hangman;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,9 +11,11 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class SignUpController {
+public class SignUpController implements Initializable {
     private UserService userService = new UserService();
 
     @FXML
@@ -61,6 +64,10 @@ public class SignUpController {
         } else {
             lblLoginError.setText("The username is taken");
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         lblLoginError.setText("");
     }
 }
